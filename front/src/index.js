8f92bbb6618fce2f5cae3c683a9dfee96e8df551
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-18 17:02:35 +0900

    MetaMask連携ボタンをAstarに対応

--- front/src/index.js
+++ front/src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Astar } from "@thirdweb-dev/chains";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider>
+    <ThirdwebProvider activeChain={Astar}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
