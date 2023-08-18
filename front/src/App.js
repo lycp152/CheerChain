8f847838f2a48a5b2d9a61a81cf501dddf541b25
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-18 21:03:17 +0900

    StartProjectのルーティング

--- front/src/App.js
+++ front/src/App.js
@@ -3,9 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./header/Header";
 import HomeFeed from "./feed/HomeFeed";
+import StartProject from "./StartProject";
+import { useConnectionStatus } from "@thirdweb-dev/react";
 
 const App = () => {
   const [isLogin, setIsLoggedIn] = useState(true);
+  const connectionStatus = useConnectionStatus();
+
   return (
     <div className="app">
       <Router>
@@ -13,7 +17,9 @@ const App = () => {
         <main className="app-main">
           <Routes>
             <Route path="/" element={<HomeFeed />} />
-            <Route path="/start" element={<HomeFeed />} />
+            {connectionStatus === "connected" && (
+              <Route path="/startproject" element={<StartProject />} />
+            )}
           </Routes>
         </main>
         {/* <footer className="app-footer">
