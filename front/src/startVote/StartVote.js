ebf8417c5a899a0059ded7b322ee4af0ec1a4362
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-19 05:05:49 +0900

    投票作成画面の入力内容をformDataに

--- front/src/startVote/StartVote.js
+++ front/src/startVote/StartVote.js
@@ -2,34 +2,30 @@ import React, { useState } from "react";
 import "./StartVote.css";
 
 const StartVote = () => {
-  const [content, setContent] = useState("");
-  const [voteOptions, setVoteOptions] = useState("");
-  const [rewardAmount, setRewardAmount] = useState("");
-  const [recruitmentCount, setRecruitmentCount] = useState("");
+  const [formData, setFormData] = useState({
+    content: "",
+    voteOptions: "",
+    rewardAmount: "",
+    recruitmentCount: "",
+  });
 
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
-  };
-
-  const handleVoteOptionsChange = (e) => {
-    setVoteOptions(e.target.value);
-  };
-
-  const handleRewardAmountChange = (e) => {
-    setRewardAmount(e.target.value);
-  };
-
-  const handleRecruitmentCountChange = (e) => {
-    setRecruitmentCount(e.target.value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newVote = {
-      content,
-      voteOptions: voteOptions.split(",").map((option) => option.trim()),
-      rewardAmount: parseFloat(rewardAmount),
-      recruitmentCount: parseInt(recruitmentCount), // 募集する投票数を整数に変換
+      ...formData,
+      voteOptions: formData.voteOptions
+        .split(",")
+        .map((option) => option.trim()),
+      rewardAmount: parseFloat(formData.rewardAmount),
+      recruitmentCount: parseInt(formData.recruitmentCount),
     };
     console.log(newVote);
   };
@@ -41,8 +37,9 @@ const StartVote = () => {
         <br />
         <label>質問：</label>
         <textarea
-          value={content}
-          onChange={handleContentChange}
+          name="content"
+          value={formData.content}
+          onChange={handleInputChange}
           required
           placeholder="例: どのお菓子が一番好き？"
         />
@@ -50,9 +47,10 @@ const StartVote = () => {
         <label>投票選択肢</label>
         <input
           type="text"
+          name="voteOptions"
           placeholder="選択肢をカンマで区切って入力（例: きのこ, たけのこ, すぎのこ）"
-          value={voteOptions}
-          onChange={handleVoteOptionsChange}
+          value={formData.voteOptions}
+          onChange={handleInputChange}
           required
         />
         <br />
@@ -60,8 +58,9 @@ const StartVote = () => {
         <input
           type="number"
           step="0.01"
-          value={rewardAmount}
-          onChange={handleRewardAmountChange}
+          name="rewardAmount"
+          value={formData.rewardAmount}
+          onChange={handleInputChange}
           required
           placeholder="0.01"
         />
@@ -69,8 +68,9 @@ const StartVote = () => {
         <label>募集する投票数：</label>
         <input
           type="number"
-          value={recruitmentCount}
-          onChange={handleRecruitmentCountChange}
+          name="recruitmentCount"
+          value={formData.recruitmentCount}
+          onChange={handleInputChange}
           required
           placeholder="100"
         />
