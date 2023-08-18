6822e4091cd7c77637dfd2bc688dd5d77bd64038
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-19 02:28:27 +0900

    投票の入力項目に報酬のAsterの量を小数で入力

--- front/src/startVote/StartVote.js
+++ front/src/startVote/StartVote.js
@@ -4,6 +4,7 @@ import "./StartVote.css"; // スタイルを適用するためのスタイルシ
 const StartVote = () => {
   const [content, setContent] = useState("");
   const [voteOptions, setVoteOptions] = useState("");
+  const [rewardAmount, setRewardAmount] = useState("");
 
   const handleContentChange = (e) => {
     setContent(e.target.value);
@@ -13,9 +14,19 @@ const StartVote = () => {
     setVoteOptions(e.target.value);
   };
 
+  const handleRewardAmountChange = (e) => {
+    setRewardAmount(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // フォームのデータを使用して新しい投稿を作成する処理を追加
+    const newVote = {
+      content,
+      voteOptions: voteOptions.split(",").map((option) => option.trim()),
+      rewardAmount: parseFloat(rewardAmount), // 仮想通貨の量を浮動小数点数に変換
+    };
+    console.log(newVote); // 作成した投票をログに表示
   };
 
   return (
@@ -26,12 +37,21 @@ const StartVote = () => {
         <label>質問：</label>
         <textarea value={content} onChange={handleContentChange} required />
         <br />
-        <label>投票選択肢：</label>
+        <label>投票選択肢</label>
         <input
           type="text"
           placeholder="選択肢をカンマで区切って入力（例: 選択肢1, 選択肢2, 選択肢3）"
           value={voteOptions}
           onChange={handleVoteOptionsChange}
+          required
+        />
+        <br />
+        <label>報酬(ASTR)：</label>
+        <input
+          type="number"
+          step="0.01" // 小数を許可するためのステップ値
+          value={rewardAmount}
+          onChange={handleRewardAmountChange}
           required
         />
         <br />
